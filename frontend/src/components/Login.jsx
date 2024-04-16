a77d4777d1be551ab66794718c20b1eb7766d0b4
VitalMangal <rvi90@bk.ru>
2024-04-16 18:06:40 +0500

    trouble with Formik

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,51 +1,60 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import logo from '../pictures/loginImg.jpeg';
+import * as Yup from 'yup';
+
+const formSchema = Yup.object().shape({
+	username: Yup.string().required('Обязательное поле'),
+	email: Yup.string().required('Обязательное поле').email('Неверный email'),
+});
 
 const LoginForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      username: '',
-      email: '',
-    },
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
 
   return (
-    <form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
-      <h1 className="text-center mb-4">Войти</h1>
-			<div className="form-floating mb-3">
-      	<label htmlFor="username">Ваш ник</label>
-      	<input
-        	id="username"
-        	name="username"
-        	type="text"
-        	onChange={formik.handleChange}
-        	value={formik.values.username}
-					autocomplete="username"
-					required=""
-					placeholder="Ваш ник"
-					className="form-control"
-      	/>
-			</div>
-			<div className="form-floating mb-4">
-      	<label htmlFor="password" className="form-label">Пароль</label>
-      	<input
-        	id="password"
-        	name="password"
-        	type="password"
-        	onChange={formik.handleChange}
-        	value={formik.values.password}
-					autocomplete="current-password"
-					required=""
-					placeholder="Пароль"
-					className="form-control"
-      	/>
-			</div>
-      <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
-    </form>
+		<Formik
+		  initialValues={{
+      	username: '',
+      	email: '',
+    	}}
+			validationSchema={formSchema}
+      onSubmit={ (values) => {
+        console.log(values);
+      }}
+    >
+			{({ errors, touched }) => (
+				<Form className="col-12 col-md-6 mt-3 mt-mb-0">
+      		<h1 className="text-center mb-4">Войти</h1>
+					<div className="form-floating mb-3">
+						<label htmlFor="username">Ваш ник</label>
+						<Field
+						id="username"
+						name="username"
+						type="text"
+						autocomplete="username"
+						required=""
+						placeholder="Ваш ник"
+						className="form-control"
+						/>
+						{errors.username && touched.username ? (
+							<div>{errors.username}</div>
+						) : null}
+					</div>
+					<div className="form-floating mb-4">
+						<label htmlFor="password" className="form-label">Пароль</label>
+      			<Field
+							id="password"
+							name="password"
+							type="password"
+							autocomplete="current-password"
+							required=""
+							placeholder="Пароль"
+							className="form-control"
+						/>
+					</div>
+      		<button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
+    		</Form>
+			)}
+    </Formik>
   );
 };
 
