f02051c6b52113337d0cc55b9365ad44fafdebfe
VitalMangal <Vitaliy.rusinov90@gmail.com>
2024-06-04 10:44:12 +0500

    step 11 completed

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 import i18next from './i18next.js';
 import AuthProvider from './context/AuthProvider.js';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react'; 
 
 import App from './App.js';
 import { store } from './redux';
@@ -11,17 +12,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './style.css'
 
+const rollbarConfig = {
+  accessToken: 'b104ab23ed4e4a769a155487b1854c29',
+  environment: 'testenv',
+}
+
+function TestError() {
+  const a = null
+  return a.hello()
+}
+
 export default () => {
   const root = ReactDOM.createRoot(document.getElementById('chat'));
   root.render(
     <React.StrictMode>
-      <Provider store={store}>
-        <I18nextProvider i18n={i18next} defaultNS={'translation'}>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
-        </I18nextProvider>
-      </Provider>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <Provider store={store}>
+            <I18nextProvider i18n={i18next} defaultNS={'translation'}>
+              <AuthProvider>
+                <TestError />
+                <App />
+              </AuthProvider>
+            </I18nextProvider>
+          </Provider>
+        </ErrorBoundary>
+      </RollbarProvider>
     </React.StrictMode>
   );
 };
+
