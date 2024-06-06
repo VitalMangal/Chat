f407aae3db9788eae74e332bd340b6a5ab408c46
VitalMangal <Vitaliy.rusinov90@gmail.com>
2024-06-06 12:39:15 +0500

    auto

--- frontend/src/components/MainPage/ChannelsComponent/ChannelsComponent.js
+++ frontend/src/components/MainPage/ChannelsComponent/ChannelsComponent.js
@@ -51,7 +51,7 @@ const ChannelsComponent = ({ activeChannelId, setActiveChannelId }) => {
               <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
               <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
             </svg>
-            <span className="visually-hidden">(t('channels.plus'))</span>
+            <span className="visually-hidden">+</span>
           </button>
         </div>
         <ButtonToolbar vertical="true" id="channels-box" aria-label="" className="flex-column mb-3 px-2 text-truncate h-100 overflow-auto">
